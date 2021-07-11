cb851769e6d0594002d5568a111bca0648bbe304
uzairriaz <uzairriaz.pk@gmail.com>
2021-07-11 11:42:54 +0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Umair Riaz on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
